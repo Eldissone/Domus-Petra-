cec31a044d55cb8634cc282faa373f8377028902
Eldissone Vilonga <eldissonev@gmail.com>
2024-07-25 21:12:12 +0100

    update in frontend

--- frontend/assets/blog/viws.js
+++ frontend/assets/blog/viws.js
@@ -1,30 +1,30 @@
-let titulo = document.querySelectorAll(".titulo")
-let texto = document.querySelector(".texto")
-let img = document.querySelector(".imgT")
+let titulo = document.querySelectorAll(".titulo");
+let texto = document.querySelector(".texto");
+let img = document.querySelector(".imgT");
 
-if (document.readyState == "loading"){
+if (document.readyState == "loading") {
     
     function checar () {
-        const Card = localStorage.getItem("Card")
-        const CardObjeto = JSON.parse(Card)
+        const Card = localStorage.getItem("Card");
+        const CardObjeto = JSON.parse(Card);
 
         if (CardObjeto) {
 
-           titulo[0].innerText = CardObjeto.titulo
-           titulo[1].innerText = CardObjeto.titulo
-           texto.innerText = CardObjeto.texto
-           let top = document.querySelector("header")
+           titulo[0].innerText = CardObjeto.titulo;
+           titulo[1].innerText = CardObjeto.titulo;
+           texto.innerText = CardObjeto.texto;
+           let top = document.querySelector(".hed-tit");
 
-           top.style.backgroundImage = `url(${CardObjeto.img})`
-           top.style.backgroundSize ="cover"
-            console.log(titulo)
+           top.style.backgroundImage = `url(${CardObjeto.img})`;
+           top.style.backgroundSize ="cover";
+           top.style.backgroundPosition ="50% 50%";
+           top.style.backgroundRepeat ="no-repeat";
+           console.log(titulo)
         
             
         } else {
             console.log("deu ruim");
         }
-
-
     }       
     
     checar ();
@@ -32,5 +32,5 @@ if (document.readyState == "loading"){
 
 else{
 
-    alert("nao carregou")
+    alert("nao carregou");
 }
