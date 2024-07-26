79fc392f7780c257ea769f718014c855549f5f62
Eldissone Vilonga <eldissonev@gmail.com>
2024-07-26 22:14:06 +0100

    update no site geral

--- frontend/assets/blog/viws.js
+++ frontend/assets/blog/viws.js
@@ -6,8 +6,14 @@ let btnComent = document.querySelector("#Comentar");
 let ComentDiv = document.querySelector(".sec-comentarios");
 
 btnComent.addEventListener("click", ()=> {
-    ComentDiv.classList.toggle("active")
-    console.log("clio");
+    logarD.style.display = "flex";
+    if (logarD) {
+        alert("preencher campo")
+
+    }else {
+        ComentDiv.classList.toggle("active")
+    }
+
 })
 
 if (document.readyState == "loading") {
@@ -27,11 +33,10 @@ if (document.readyState == "loading") {
            top.style.backgroundSize ="cover";
            top.style.backgroundPosition ="50% 50%";
            top.style.backgroundRepeat ="no-repeat";
-           console.log(titulo)
         
             
         } else {
-            console.log("deu ruim");
+            console.log("sem dados clicado");
         }
     }       
     
@@ -42,3 +47,171 @@ else{
 
     alert("nao carregou");
 }
+
+
+const loginForm = document.querySelector(".login_form")
+const loginInput = document.querySelector("#name")
+let logarD = document.querySelector(".logar");
+
+const chat = document.querySelector(".sec-comentarios")
+const chatForm = document.querySelector(".coment")
+const chatInput = document.querySelector("#text-coment")
+const chat_messages = document.querySelector(".chat-coment")
+
+
+let userSMS = new Array(); 
+
+const colors = [
+    "cadeblue",
+    "orange",
+    "green",
+    "brown",
+    "red",
+    "hotpink",
+    "gold",
+    "#ff6f00",
+    "#e0dfdf"
+]
+
+const user = { id: "", name: "", color: "" }
+
+let websocket
+
+const createMessageOtherElement = (content, sender, senderColor) => {
+    
+    div = document.createElement("div");
+    const span = document.createElement("span");
+    const p = document.createElement("p");
+
+    div.classList.add("coment-user");
+    span.classList.add("user-name-coment");
+    p.classList.add("txt-coment");
+    span.style.color = senderColor;
+
+    div.appendChild(span);
+    div.appendChild(p);
+
+    span.innerHTML = sender;
+    p.innerHTML += content;
+
+    return div;
+
+}
+
+const createMessageSelfElement = (content, sender) => {
+
+    div = document.createElement("div");
+    const span = document.createElement("span");
+    const p = document.createElement("p");
+
+    div.classList.add("coment-user");
+    span.classList.add("user-name-coment");
+    p.classList.add("txt-coment");
+
+    div.appendChild(span);
+    div.appendChild(p);
+
+    span.innerHTML = user.name;
+    p.innerHTML += content;
+    
+    return div;
+    
+}
+
+
+const getRandomColor = () => {
+    const randomIndex = Math.floor(Math.random() * colors.length);
+    return colors[randomIndex]
+}
+
+const scrollScreen = () => {
+    window.scrollTo({
+        top: document.body.scrollHeight,
+        behavior: "smooth"
+    })
+}
+
+const processMessage = ({data}) => {
+
+    const {userId, userName, userColor, content} = (JSON.parse(data));
+
+    const message = userId == user.id 
+    ? createMessageSelfElement(content)
+    : createMessageOtherElement(content, userName, userColor);
+
+
+    chat_messages.appendChild(message);
+    scrollScreen();
+}
+
+
+const handleLogin = (event) => {
+    event.preventDefault()
+
+    user.id = crypto.randomUUID();
+    user.name = loginInput.value;
+    user.color = getRandomColor();
+
+    logarD.style.display = "none";
+    chat.style.display = "block";
+
+
+    websocket = new WebSocket("wss://backend-chat-ss58.onrender.com");
+
+    websocket.onmessage = processMessage;
+
+
+}
+//localStorage.clear()
+const sendMessage = (event) => {
+    event.preventDefault();
+
+    const message = {
+        userId: user.id,
+        userName: user.name,
+        userColor: user.color,
+        content: chatInput.value
+    }
+    
+    websocket.send(JSON.stringify(message));    
+    chatInput.value = "";
+      
+    let userSMS = new Array();  
+
+    if (localStorage.hasOwnProperty("userSMS")) {
+        userSMS  = JSON.parse(localStorage.getItem("userSMS"));
+    } 
+    
+    userSMS.push(message);
+    
+    localStorage.setItem("userSMS", JSON.stringify(userSMS))
+    
+    
+    // window.location.reload()
+}
+
+
+window.addEventListener("load", (event) => {
+    event.preventDefault();
+
+    if (userSMS){
+        userSMS  = JSON.parse(localStorage.getItem("userSMS"));
+        for (let i in userSMS){
+            const {userId, userName, userColor, content} = userSMS[i];
+
+            const message = userId == user.id 
+            ? createMessageSelfElement(content)
+            : createMessageOtherElement(content, userName, userColor);
+
+            chat_messages.appendChild(message);
+
+        }
+        
+    }else {
+        console.log("sem dados de comentarios")
+    }
+});
+        
+
+loginForm.addEventListener("submit", handleLogin);
+chatForm.addEventListener("submit", sendMessage);
