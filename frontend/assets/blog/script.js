bdb6e05453dd463423da1c1f5f9c6642c074adcf
Eldissone Vilonga <eldissonev@gmail.com>
2024-07-27 00:24:30 +0100

    update

--- frontend/assets/blog/script.js
+++ frontend/assets/blog/script.js
@@ -13,3 +13,47 @@ Submit.onclick = () => {
         Error('Wrong password')
     }
 }
+ 
+// ========inicio==========
+if (document.readyState == "loading"){
+    document.addEventListener("DOMContentLoaded", ready);
+} else{
+    ready();
+}
+
+function ready () {
+    //Adicionar para a Caixa de visualizaçao
+    let viewCard = document.getElementsByClassName('card-blog');
+
+    for(var i = 0; i < viewCard.length; i++){
+        let btn = viewCard[i]
+        btn.addEventListener('click', addCardClick);
+     }
+}
+document.querySelectorAll('.card button').forEach(button => {
+    button.addEventListener('click', addCardClick);
+});
+
+function addCardClick(event) {
+    let button = event.target;
+    let CardVer = button.parentElement;
+    let titulo = CardVer.querySelector('#tit-card').innerText;
+    let texto = CardVer.querySelector('#texto').innerText;
+    let imagem = CardVer.querySelector('#img-card').src;
+
+    addCardVer (titulo, texto, imagem);
+}
+
+
+function addCardVer (titulo, texto, imagem) {
+    const Card = {
+        titulo:  titulo,
+        texto:  texto, 
+        img:  imagem, 
+    }
+
+    localStorage.setItem("Card", JSON.stringify(Card)) //transformar o objecto em string
+    
+    location.href = "visualizar.html";
+    
+}
--- frontend/assets/link.js
+++ frontend/assets/link.js
@@ -1,8 +1,18 @@
-let btnLogo = document.querySelector(".logo");
+const express = require('express');
+const mime = require('mime-types');
 
+    const app = express();
 
-btnLogo.onclick = () => { 
-window.location.href = "index.html"
-console.log("clicou no logo");
+    // ...
 
-}
+    app.use(express.static('frontend', {
+        setHeaders: (res, path) => {
+            res.setHeader('Content-Type', mime.lookup(path) || 'application/octet-stream');
+        },
+    }));
+
+// let btnLogo = document.querySelector(".logo");
+
+// btnLogo.addEventListener('click', function() {
+//     window.location.replace('/frontend/assets/');
+// }, false);
